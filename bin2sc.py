5c27c410a71fdca3c2bd3f6c751f6a265a295e36
VeryDampTowel <32224673+VeryDampTowel@users.noreply.github.com>
2024-04-04 09:51:04 -0400

    Python3 compatible

--- bin2sc.py
+++ bin2sc.py
@@ -1,36 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
+
 if __name__ == "__main__":
-	if len(sys.argv) < 3:
-		print "usage: %s file.bin c|cs\n" % (sys.argv[0],)
-		sys.exit(0)
+    if len(sys.argv) < 3:
+        print("usage: {} file.bin c|cs".format(sys.argv[0]))
+        sys.exit(0)
 
-	if sys.argv[2] == "c":
-		# for c shellcode
-		shellcode = "\""
-		ctr = 1
-		maxlen = 15
+    if sys.argv[2] == "c":
+        # for c shellcode
+        shellcode = "\""
+        ctr = 1
+        maxlen = 15
 
-		for b in open(sys.argv[1], "rb").read():
-			shellcode += "\\x" + b.encode("hex")
-			if ctr == maxlen:
-				shellcode += "\" \n\""
-				ctr = 0
-			ctr += 1
-		shellcode += "\""
-		print shellcode
+        with open(sys.argv[1], "rb") as f:
+            bytes = f.read()
+            for b in bytes:
+                shellcode += "\\x" + format(b, "02x")
+                if ctr == maxlen:
+                    shellcode += "\" \n\""
+                    ctr = 0
+                ctr += 1
+        shellcode += "\""
+        print(shellcode)
 
-	else:
-		# for cs shellcode
-		shellcode = ""
-		ctr = 1
-		maxlen = 15
-		
-		for b in open(sys.argv[1], "rb").read():
-			shellcode += "0x" + b.encode("hex") + ","
-			if ctr == maxlen:
-				shellcode += "\n"
-				ctr = 0
-			ctr += 1
+    else:
+        # for cs shellcode
+        shellcode = ""
+        ctr = 1
+        maxlen = 15
+        
+        with open(sys.argv[1], "rb") as f:
+            bytes = f.read()
+            for b in bytes:
+                shellcode += "0x" + format(b, "02x") + ","
+                if ctr == maxlen:
+                    shellcode += "\n"
+                    ctr = 0
+                ctr += 1
 
-		print shellcode
+        print(shellcode)
